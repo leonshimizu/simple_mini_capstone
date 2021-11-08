7bd0a6396491a320ef858ae1a5064713fd218dc7
“leonshimizu” <lmshimizu@gmail.com>
2021-11-07 18:34:25 -0800

    products update and delete

--- products.js
+++ products.js
@@ -2,15 +2,15 @@
 
 var app = new Vue({
   el: "#app",
-  data: function() {
+  data: function () {
     return {
       message: "Hello from JavaScript!",
       products: []
     };
   },
   methods: {
-    allProducts: function() {
-      console.log("in the index function"); 
+    allProducts: function () {
+      console.log("in the index function");
       axios
         .get('http://localhost:3000/products')
         .then(response => {
@@ -18,11 +18,11 @@ var app = new Vue({
           this.products = response.data;
         });
     },
-    createProduct: function() {
+    createProduct: function () {
       console.log("in the create function");
       axios.post('http://localhost:3000/products', {
         id: 8,
-        name: "Aloha Water",
+        name: "Juice",
         description: "Expensive but good water",
         imageUrl: "https://produits.bienmanger.com/21129-0w470h470_Fiji_Natural_Artesian_Water.jpg",
         price: 3.00,
@@ -35,11 +35,18 @@ var app = new Vue({
     },
     updateProduct: function () {
       console.log("in the update function");
-      axios.patch('http://localhost:3000/products/9', {
-        name: "Water"
+      axios.patch('http://localhost:3000/products/11', {
+        name: "Aloha Water"
       })
         .then(response => {
           console.log(response.data);
+        });
+    },
+    deleteProduct: function () {
+      console.log("in the delete function");
+      axios.delete('http://localhost:3000/products/11')
+        .then(response => {
+          console.log(response);
         });
     }
   },
@@ -47,13 +54,3 @@ var app = new Vue({
     this.allProducts();
   }
 });
-
-// axios.put('https://example.com/cats/1', {
-//     name: 'Tophat Cat'
-//   })
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(error => {
-//     console.log(err);
-//   });
