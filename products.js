d2b74766b9df7f0eb539575c06278e97724948ce
“leonshimizu” <lmshimizu@gmail.com>
2021-11-08 17:41:40 -0800

    user can type in all attributes for new product

--- products.js
+++ products.js
@@ -5,11 +5,15 @@ var app = new Vue({
   data: function () {
     return {
       message: "Hello from JavaScript!",
-      products: []
+      products: [],
+      name: "Name",
+      description: "Description",
+      imageUrl: "Image URL",
+      price: "Price"
     };
   },
   methods: {
-    allProducts: function () {
+    allProducts: function () { //index
       console.log("in the index function");
       axios
         .get('http://localhost:3000/products')
@@ -21,13 +25,13 @@ var app = new Vue({
     createProduct: function () {
       console.log("in the create function");
       axios.post('http://localhost:3000/products', {
-        id: 8,
-        name: "Juice",
-        description: "Expensive but good water",
-        imageUrl: "https://produits.bienmanger.com/21129-0w470h470_Fiji_Natural_Artesian_Water.jpg",
-        price: 3.00,
-        tax: 0.21,
-        total: 3.21
+        id: 13,
+        name: this.name,
+        description: this.description,
+        imageUrl: this.imageUrl,
+        price: this.price,
+        tax: this.price * 0.07,
+        total: this.price + (this.price * 0.07)
       }).then(response => {
         console.log(response.data);
         this.products.push(response.data);
@@ -44,7 +48,7 @@ var app = new Vue({
     },
     deleteProduct: function () {
       console.log("in the delete function");
-      axios.delete('http://localhost:3000/products/11')
+      axios.delete('http://localhost:3000/products/10')
         .then(response => {
           console.log(response);
         });
@@ -54,3 +58,15 @@ var app = new Vue({
     this.allProducts();
   }
 });
+
+// axios.post('http://localhost:3000/products', {
+//         id: 8,
+//         name: "Juice",
+//         description: "Expensive but good water",
+//         imageUrl: "https://produits.bienmanger.com/21129-0w470h470_Fiji_Natural_Artesian_Water.jpg",
+//         price: 3.00,
+//         tax: 0.21,
+//         total: 3.21
+//       }).then(response => {
+//         console.log(response.data);
+//         this.products.push(response.data);
